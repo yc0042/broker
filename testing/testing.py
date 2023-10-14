581890cb3d8fb01c2f74540ed1541f979e33ddcd
yc0042 <yash.choudhury2004@gmail.com>
2023-10-14 13:45:53 -0400

    more testing + pre existing bid case handled

--- testing/testing.py
+++ testing/testing.py
@@ -25,11 +25,26 @@ def createDummyAuction(BondId, SellerId, MaxApr):
     data = requests.post("http://localhost:8001/create_dummy_auction", data = dumps(args))
     print(data.content)
 
+def getAllAuctions():
+    data = requests.post("http://localhost:8001/get_auctions", data = dumps(dict(uuids=[])))
+    print(data.json())
 
 if __name__ == "__main__":
-    createDummyAuction(0, 0, 10000)
-    sendBid(1, 0, 2)
+    # seller: 0 creates auction for bond: 0 with maxapr: 15%
+    createDummyAuction(0, 0, 0.15)
 
-    
+    # buyer: 1 bids on bond: 0 with apr: 13%
+    sendBid(0.13, 0, 1)
+
+    # buyer: 2 bids on bond: 0 with apr: 10%
+    sendBid(0.10, 0, 2)
+
+    # seller: 3 creates auction for bond: 1 with maxapr: 12%
+    createDummyAuction(1, 3, 12)
+    getAllAuctions()
+
+    # seller: 1 attempts to create auction for bond: 0 with maxapr: -1%
+    createDummyAuction(0, 1, -0.01)
+    getAllAuctions()
 
 
